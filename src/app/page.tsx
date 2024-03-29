245febeaec66122c46fba0f323c80693b0c95864
nootanlodhi <nootancsm19028@gmail.com>
2024-03-29 17:12:38 +0530

    implement dashboard structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,73 @@
 import Image from "next/image";
+import TVImg from "./assets/images/tv-img.png";
+import HeroSectionBGImage from "./assets/images/hero-bg-img.jpg";
+import StrangerImg from "./assets/images/mobile-stranger-img.jpg";
+import StrangerThings from "./assets/images/stranger-things.png";
+import DownloadIcon from "./assets/images/download-icon.gif";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="relative h-[43rem]">
+      <div className="w-full absolute z-10 flex justify-between items-center xs:px-4 md:px-8 xl:px-64 py-4">
+        <h1 className="xs:text-[1.5rem] sm:text-[3rem] text-red font-bold">NETFLIX</h1>
+        <div className="flex gap-5 items-center">
+          <select className="px-4 py-2 bg-light-black text-white border border-gray rounded">
+            <option>English</option>
+            <option>Hindi</option>
+          </select>
+          <button className="bg-red text-white px-2 py-1 h-1/2 border border-red rounded">Sign In</button>
+        </div>
+      </div>
+      <div className="w-full h-full overflow-hidden relative">
+        <Image className="w-full h-full object-cover" src={HeroSectionBGImage} alt=""/>
+        <div className="absolute top-0 bottom-0 left-0 right-0 background text-center flex flex-col justify-center items-center gap-3 text-white xs:px-5 sm:px-16 md:px-10">
+          <h1 className="xs:text-4xl md:text-5xl font-extrabold">Unlimited movies, TV shows and more</h1>
+          <p className="xs:text-xl md:text-2xl">Watch anywhere. Cancel anytime.</p>
+          <p className="text-xl">Ready to watch? Enter your email to create or restart your membership.</p>
+          <div className="w-full flex xs:flex-col md:flex-row justify-center items-center gap-4">
+            <input className="xs:w-full md:w-7/12 lg:w-5/12 xl:w-4/12 xxl:w-1/5 px-5 py-3.5 bg-light-black border border-gray rounded" placeholder="Email address"/>
+            <button className="bg-red text-2xl font-bold px-7 py-2 border border-red rounded">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <div className="w-full bg-dark-gray h-full py-2">
+        <div className="w-full h-full bg-black text-white grid xs:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 text-center xs:px-5 lg:px-30 xl:px-64 xxl:px-80">
+          <div className="text-white flex flex-col justify-center xs:text-center lg:text-left">
+            <h1 className="xs:text-4xl md:text-5xl font-extrabold">Enjoy on your TV</h1>
+            <p className="xs:text-xl md:text-2xl">Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</p>
+          </div>
+          <div className="w-full h-full text-white flex justify-center items-center">
+            <div className="relative">
+              <Image src={TVImg} alt="" className=""/>
+              <video width={"73%"} loop muted autoPlay className="absolute top-[20.7%] left-[13.1%] right-[50%] bottom-[50%]">
+                <source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v" type="video/mp4"/>
+              </video>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-full bg-dark-gray h-full">
+        <div className="w-full h-full bg-black text-white grid xs:grid-cols-reverse-1 md:grid-cols-1 lg:grid-cols-2 text-center xs:px-5 lg:px-30 xl:px-64 xxl:px-80">
+          <div className="w-full h-full text-white flex justify-center items-center xs:order-last lg:order-first">
+            <div className="relative">
+              <Image className="" src={StrangerImg} alt=""/>
+              <div className="xs:w-9/12 sm:w-6/12 flex items-center justify-between border-2 border-dark-gray rounded-lg p-3 bg-black absolute top-[72%] xs:left-[13%] sm:left-[25%]">
+                <Image className="max-w-[20%] max-h-full object-contain" src={StrangerThings} alt=""/>
+                <div>
+                  <p>Stranger Things</p>
+                  <p className="text-blue">Downloading...</p>
+                </div>
+                <Image className="max-w-[20%] max-h-full object-contain" src={DownloadIcon} alt=""/>
+              </div>
+            </div>
+          </div>
+          <div className="text-white flex flex-col justify-center xs:text-center lg:text-left xs:order-first lg:order-last">
+            <h1 className="xs:text-4xl md:text-5xl font-extrabold">Download your shows to watch offline</h1>
+            <p className="xs:text-xl md:text-2xl">Save your favourites easily and always have something to watch.</p>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,39 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      xs: '100px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+      xxl: '1780px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'red': '#e50914',
+      'black': '#000000',
+      'light-black': '#161616b3',
+      'white': '#ffffff',
+      'gray': '#808080b3',
+      'dark-gray': '#525252',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
+    
   },
   plugins: [],
 };
